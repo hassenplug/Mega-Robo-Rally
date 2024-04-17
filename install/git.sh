e8248d5eef7b5f9d55aaa0f74ee64e0c4483b224
Steve Hassenplug <38286851+hassenplug@users.noreply.github.com>
2024-04-17 14:10:08 -0400

    Update git.sh

--- install/git.sh
+++ install/git.sh
@@ -2,4 +2,4 @@ sudo apt install git
 
 git clone https://github.com/hassenplug/Mega-Robo-Rally.git
 
-/home/mrr/server/MRR_CLG/install/installMRR.sh
+/home/mrr/Mega-Robo-Rally/MRR_CLG/install/installMRR.sh
